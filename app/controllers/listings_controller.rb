0a8c7886affb7098d4ceadd8597de10a2093a295
Alex Diwa <atdiwa@gmail.com>
2019-04-18 12:14:44 +1000

    add create method for listings controller and whitelisted listing params

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,6 +12,9 @@ class ListingsController < ApplicationController
   end
 
   def create
+    #whitelisted params as new_params
+    @listing = Listing.create(listing_params)
+    byebug
   end
 
   def update
@@ -31,5 +34,9 @@ class ListingsController < ApplicationController
   def set_listing
     id = params[:id]
     @listing = Listing.find(id)
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet)
   end
 end
