213af92acded91f915532116add76e1ba4751dec
Alex Diwa <atdiwa@gmail.com>
2019-04-18 12:40:39 +1000

    added in logic for create method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_breeds_and_sexes, only: [:new, :edit]
 
   def index
     @listings = Listing.all
@@ -7,14 +8,18 @@ class ListingsController < ApplicationController
 
   def new
     @listing = Listing.new
-    @breeds = Breed.all
-    @sexes = Listing.sexes.keys
   end
 
   def create
     #whitelisted params as new_params
     @listing = Listing.create(listing_params)
-    byebug
+    
+    if @listing.errors.any?
+      set_breeds_and_sexes
+      render "new"
+    else
+      redirect_to listings_path
+    end
   end
 
   def update
@@ -30,6 +35,11 @@ class ListingsController < ApplicationController
   end
 
   private
+
+  def set_breeds_and_sexes
+    @breeds = Breed.all
+    @sexes = Listing.sexes.keys
+  end
 
   def set_listing
     id = params[:id]
