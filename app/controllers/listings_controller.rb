b934d511476bc83f966efc7ece6a52f344b6810c
Alex Diwa <atdiwa@gmail.com>
2019-04-23 15:16:39 +1000

    authentication and authorization configuration

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!  
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
+  # before_action :set_user_listing, only: [:edit, :update, :destroy]
   before_action :set_breeds_and_sexes, only: [:new, :edit]
 
   def index
@@ -13,7 +15,8 @@ class ListingsController < ApplicationController
 
   def create
     #whitelisted params as new_params
-    @listing = Listing.create(listing_params)
+    @listing = current_user.listings.create(listing_params)
+    # @listing = Listing.create(listing_params)
     
     if @listing.errors.any?
       set_breeds_and_sexes
@@ -46,6 +49,16 @@ class ListingsController < ApplicationController
     id = params[:id]
     @listing = Listing.find(id)
   end
+
+  def authorize_user
+    redirect_to listings_path if @listing.user_id != current_user.id
+  end
+
+  # def set_user_listing
+  #   id = params[:id]
+  #   @listing = Listing.where(user_id: current_user.id, id: id).first()
+  #   redirect_to listings_path if !@listing
+  # end
 
   def listing_params
     params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
