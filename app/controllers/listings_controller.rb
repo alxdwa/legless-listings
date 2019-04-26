cb5c643ab5157c7d0fa8b628e624bdd9a05bf573
Alex Diwa <atdiwa@gmail.com>
2019-04-26 15:28:37 +1000

    adding payment functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -36,6 +36,25 @@ class ListingsController < ApplicationController
   end
 
   def show
+    stripe_session = Stripe::Checkout::Session.create(
+    payment_method_types: ['card'],
+    client_reference_id: current_user.id,
+    line_items: [{
+    name: @listing.title,
+    description: @listing.description,
+    amount: @listing.deposit,
+    currency: 'aud',
+    quantity: 1,
+    }],
+    payment_intent_data: {
+      metadata: {
+        listing_id: @listing.id
+      }
+    },
+      success_url: 'http://localhost:3000/payments/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    )
+    @stripe_session_id = stripe_session.id
   end
 
   private
